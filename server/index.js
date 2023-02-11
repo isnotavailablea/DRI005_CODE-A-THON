92786612a30c3e218ed70230cd6da9a93a1fc4e5
isnotavailablea <97468582+isnotavailablea@users.noreply.github.com>
2023-02-12 03:34:19 +0530

    Updated index.js entry file with new image routes

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const { response } = require('express')
 const exp=require('express')
 const app=exp()
 const cors=require('cors')
+const bodyParser = require("body-parser");
+
 const mongo=require('mongoose')
 // const =require('./Routes/usersignup')
 const identification = require('./Routes/usersignup')
@@ -13,10 +15,14 @@ const addfestival=require('./Routes/addfestival')
 const givefests=require('./Routes/getfestivals')
 const addblogs = require('./Routes/addblog')
 const getblogs = require('./Routes/getblogs')
+const addimgfests = require('./Routes/addimgforfests')
+const givefestimgs = require('./Routes/getfestimgs')
 const port ='8000'
 mongo.connect(process.env.CONNECTION_URL)
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(exp.json())
-app.use(exp.urlencoded({extended:true}))
+// app.use(exp.urlencoded({extended:true}))
 app.use(cors())
 app.use('/signup',identification)
 app.use('/login',finduser)
@@ -26,6 +32,8 @@ app.use('/addfest',addfestival)
 app.use('/getfests',givefests)
 app.use('/addblog',addblogs)
 app.use('/getblogs',getblogs)
+app.use('/addfestimg',addimgfests)
+app.use('/getfestimgs',givefestimgs)
 app.listen(port,()=>{
     console.log(`${port} is listening`);
 })
